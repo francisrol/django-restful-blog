2f32dd23b1bee623a180fc9361c284cef897ef74
francisrol <whiteHouse@Francis.local>
2017-07-16 20:45:24 +0800

    更改为mysql服务器

--- blogWebsite/blogWebsite/settings.py
+++ blogWebsite/blogWebsite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'blogWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": 'django.db.backends.mysql',
+        "NAME": "blogWebsite",
+        "USER": "website",
+        "PASSWD": "website",
+        "HOST": "localhost",
+        "PORT": 3306,
     }
 }
 
