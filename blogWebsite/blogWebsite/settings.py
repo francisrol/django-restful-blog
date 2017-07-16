16312d8c76d91c7bd26dbb6e6c4a77f035295b5c
francisrol <whiteHouse@Francis.local>
2017-07-17 00:49:50 +0800

    allowed hosts

--- blogWebsite/blogWebsite/settings.py
+++ blogWebsite/blogWebsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hello world'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0',]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
